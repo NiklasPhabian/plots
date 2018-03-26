58348b48a0c0a08a6eeb2ffff6bce21ed51f879e
NiklasPhabian <griessbaum@gmail.com>
2018-03-26 10:30:28 -0700

    restructured

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
- 
+from setuptools import setup
+
+setup(name='plots',
+      version='0.1',
+      description='Matplotlib abstraction to make plotting easier',
+      url='https://github.com/NiklasPhabian/plots',
+      author='Niklas Griessbaum',      
+      license='MIT',
+      packages=['plots'],
+      zip_safe=False)
