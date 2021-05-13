979f3b18431526b6e9792524c01e904a72bec3fc
niklasphabian <griessbaum@gmail.com>
2021-05-12 18:24:04 -0700

    updates

--- plots.py
+++ plots.py
@@ -4,24 +4,18 @@ from matplotlib import cm
 import matplotlib.dates
 import numpy
 import math
+import pandas
+from matplotlib.patches import Circle
 
-# Configure to always fore latex and siuintx
-plt.rcParams['text.latex.preamble'] = [
-    r'\usepackage{siunitx}',    # i need upright \micro symbols, but you need...
-    r'\sisetup{detect-all}',    # ...this to force siunitx to actually use your fonts
-    r'\usepackage{helvet}',     # set the normal font here
-    r'\usepackage{sansmath}',   # load up the sansmath so that math -> helvet
-    r'\sansmath'  # <- tricky! -- gotta actually tell tex to use!
-]
-
-plt.rc('text', usetex=True)
 font_size = 10
 
 
 class Plot:
-    def __init__(self, width_inch=10, width_pixel=None, dpi=300):        
-        width = width_inch
-        height = width / (1 + 5**0.5) * 2
+    def __init__(self, width=10, height=None, dpi=300):        
+        # Width in inch?
+        if height is None:
+            height = width / (1 + 5**0.5) * 2
+        
         self.fig, self.ax = plt.subplots(nrows=1, ncols=1, figsize=(width, height), dpi=dpi)
         plt.subplots_adjust(left=0.15, bottom=0.15, right=0.9, top=None, wspace=None, hspace=None)
         self.ax2 = None
@@ -29,7 +23,20 @@ class Plot:
         self.legend = []
         self.legend_loc = None
         self.lines = []
-        self.marker_size = 0
+        self.marker_size = None
+        
+    def activate_tex():
+        # Configure to always fore latex and siuintx
+        #plt.rcParams['text.latex.preamble'] = [
+        #    r'\usepackage{siunitx}',    # i need upright \micro symbols, but you need...
+        #    r'\sisetup{detect-all}',    # ...this to force siunitx to actually use your fonts
+        #    r'\usepackage{helvet}',     # set the normal font here
+        #    r'\usepackage{sansmath}',   # load up the sansmath so that math -> helvet
+        #    r'\sansmath'                # <- tricky! -- gotta actually tell tex to use!
+        #]
+        preamble = r'''\usepackage{siunitx} \sisetup{detect-all} \usepackage{helvet} \usepackage{sansmath} \sansmath'''
+        plt.rc('text', usetex=True)
+        plt.rc('text.latex', preamble=preamble)
         
     def set_marker_size(self, marker_size):
         self.marker_size = marker_size
@@ -40,15 +47,21 @@ class Plot:
         self.legend.append(series_name)
         self.set_xlim(min(x), max(x))
 
-    def plot_pandas(self, series, linestyle='solid', marker=None, series_name=None):
-        line = self.ax.plot(series, linestyle=linestyle, marker=marker, markersize=self.marker_size)[0]
+    def plot_pandas(self, series, linestyle='solid', marker=None, series_name=None, **kwargs):
+        line = self.ax.plot(series, linestyle=linestyle, marker=marker, markersize=self.marker_size, **kwargs)[0]
         self.x_label(series.index.name)
         self.lines.append(line)
-        self.legend.append(series.name)
+        if series_name:
+            self.legend.append(series_name)
+        else:
+            self.legend.append(series.name)
         
-    def plot_pandas_df(self, dataframe, linestyle='solid', marker='None'):        
-        self.ax = dataframe.plot(ax=self.ax, grid=True, linestyle=linestyle, marker=marker, markersize=20)        
+    def plot_pandas_df(self, dataframe,  **kwargs):  
+        self.ax = dataframe.plot(ax=self.ax, grid=True, **kwargs)
         self.legend.append(dataframe.columns)
+        
+    def plot_pandas_series(self, series,  **kwargs):  
+        self.ax = series.plot(ax=self.ax, grid=True, **kwargs)
 
     def setup_plot(self):
         self.ax.grid(True)
@@ -122,12 +135,21 @@ class Plot:
         
     def set_axis_timezone(self, tz):
         plt.gca().xaxis_date(tz)
+    
+    def set_hourly_ticks(self):
+        self.ax.xaxis.set_major_locator(matplotlib.dates.HourLocator())
 
     def set_daily_ticks(self):
         self.ax.xaxis.set_major_locator(matplotlib.dates.DayLocator())
         
+    def set_monthly_ticks(self):
+        self.ax.xaxis.set_major_locator(matplotlib.dates.MonthLocator())
+       
     def add_text_ticks(self, labels):
         plt.xticks(numpy.arange(len(labels)), labels)
+    
+    def set_xticks(self, ticks):
+        self.ax.set_xticks(ticks)
         
     def rotate_ticks(self, rotation):
         plt.xticks(rotation=rotation)
@@ -170,12 +192,22 @@ class Plot:
       
     def y_log(self):
         self.ax.set_yscale('log')
+    
         
             
 class Histogram(Plot):
   
   def plot_pandas(self, series, bins=10):
       series.hist(ax=self.ax, bins=bins)      
+
+class Barplot(Plot):
+    
+    def plot_pandas_df(self, df, x, y):
+        self.ax = df.plot(x=x, y=y, ax=self.ax, grid=True, color='b', kind='bar')
+        
+    def plot_pandas(self, series):
+        self.ax = df.plot(x=x, y=y, ax=self.ax, grid=True, color='b', kind='bar')
+            
 
 class GeoPlot(Plot):
 
@@ -209,8 +241,9 @@ class Plot3D(Plot):
 class SpherePlot:
     def __init__(self, radius):
         self.radius = radius
-        self.fig = plt.figure(figsize=(15, 15), dpi=300)
-        self.ax = self.fig.add_subplot(111, projection='3d')
+        self.fig = plt.figure(figsize=[10, 10])
+        #self.fig, self.ax = plt.subplots(nrows=1, ncols=1, figsize=(100, 100), dpi=300)
+        self.ax = self.fig.add_subplot(111, projection='3d', xlim=[-radius,radius], ylim=[-radius, radius])
         self.color_counter = 1
         self.current_color = None
         self.boundaries = None
@@ -237,7 +270,7 @@ class SpherePlot:
     def set_color(self, color):
         self.current_color = color
 
-    def make_sphere(self, finess=20, wire=False, zoom=1, only_north=False, only_front=False):
+    def make_sphere(self, finess=20, wire=False, zoom=1, only_north=False, only_front=False, alpha=0.5):
         finess = complex(0, finess)
         lat, lon = numpy.mgrid[-numpy.pi / 2:numpy.pi / 2:finess, 0:2 * numpy.pi:finess]
 
@@ -250,19 +283,34 @@ class SpherePlot:
         y = (self.radius * numpy.cos(lat) * numpy.sin(lon)) * zoom
         z = (self.radius * numpy.sin(lat)) * zoom
         if wire:
-            self.ax.plot_wireframe(x, y, z, rstride=1, cstride=1, color='black', alpha=0.5, linewidth=self.line_width)
+            self.ax.plot_wireframe(x, y, z, rstride=1, cstride=1, color='black', alpha=alpha, linewidth=self.line_width)
         else:
             self.ax.plot_surface(x, y, z, rstride=1, cstride=1, color='c', alpha=1, linewidth=0.1, shade=False)
+            
+    def make_tangent_plane(self, normal):
+        point = numpy.array(normal) 
+        point = point / numpy.linalg.norm(point) * self.radius
+        self.make_plane(normal, point)
+        
+    def make_divider(self, normal,  **kwargs):
+        point = [0, 0, 0]
+        self.make_plane(normal, point,  **kwargs)
+        
+    def make_gc_divider(self, normal, **kwargs):
+        self.plot_great_circle(normal, radius=self.radius, fill=True, **kwargs)
 
-    def make_divider(self):
-        normal = numpy.array([0, 0, self.radius])
-        point = numpy.array([0, 0, 0])
+    def make_plane(self, normal, point, wire=False, surf=True,  **kwargs):
+        normal = numpy.array(normal)
+        point = numpy.array(point)
         d = -point.dot(normal)
         xx, yy = numpy.meshgrid(range(-1, 2), range(-1, 2))
         xx = xx * numpy.float64(self.radius)
         yy = yy * numpy.float64(self.radius)
         z = (-normal[0] * xx - normal[1] * yy - d) * 1. / normal[2]
-        self.ax.plot_surface(xx, yy, z, rstride=1, cstride=1, color='black', alpha=1, linewidth=1, shade=False)
+        if surf:
+            self.ax.plot_surface(xx, yy, z, rstride=1, cstride=1,  **kwargs)
+        if wire:
+            p.ax.plot_wireframe(xx, yy, z, rcount=100, ccount=100, **kwargs)
 
     def plot_point_coordinates(self, lat, lon):
         x, y, z = self.coord_to_carthesian(lat, lon)
@@ -317,13 +365,24 @@ class SpherePlot:
 
     def nvector_to_pvector(self, nvector):
         return nvector * self.radius
+    
+    def plot_nvector(self, vector, scale=1, **kwargs):
+        if 'color' not in kwargs:
+            kwargs['color'] = self.current_color
+        if 'linewidth' not in kwargs:
+            kwargs['linewidth'] = self.line_width      
+        vector = vector / numpy.linalg.norm(vector) * scale
+        self.ax.plot(xs=(0, vector[0]), 
+                     ys=(0, vector[1]),
+                     zs=(0, vector[2]),
+                     alpha=1, **kwargs)
 
-    def plot_straight_line_from_nvector(self, nvector_a, nvector_b):
+    def plot_straight_line_from_nvectors(self, nvector_a, nvector_b):
         pvector_a = self.nvector_to_pvector(nvector_a)
         pvector_b = self.nvector_to_pvector(nvector_b)
         self.plot_straight_line_from_pvector(pvector_a, pvector_b)
 
-    def plot_straight_line_from_pvector(self, vector_a, vector_b):
+    def plot_straight_line_from_pvectors(self, vector_a, vector_b):
         if self.geopoint_inside_view(vector_a) or self.geopoint_inside_view(vector_b):
             self.ax.plot(xs=(vector_a[0], vector_b[0]),
                          ys=(vector_a[1], vector_b[1]),
@@ -343,6 +402,11 @@ class SpherePlot:
     def set_view(self, elevation, azimuth):
         # Elev in degrees (i.e. latitude)
         self.ax.view_init(elev=elevation, azim=azimuth)
+        
+    def set_axis_lim(self, ax_lim):
+        self.ax.set_xlim3d(-ax_lim, ax_lim)
+        self.ax.set_ylim3d(-ax_lim, ax_lim)
+        self.ax.set_zlim3d(-ax_lim, ax_lim)
 
     def zoom_to(self, lat, lon, field_size, elevation=None, set_azimuth=False):
         if not elevation:
@@ -377,20 +441,86 @@ class SpherePlot:
         else:
             return True
 
-    def default_zoom(self):
-        zoom = 1
+    def default_zoom(self, zoom=1):        
         self.ax.set_xlim([-self.radius / zoom, self.radius / zoom])
         self.ax.set_ylim([-self.radius / zoom, self.radius / zoom])
         self.ax.set_zlim([-self.radius / zoom, self.radius / zoom])
 
     def save_fig(self, filename):
-        self.ax.set_aspect('equal')
+        #self.ax.set_aspect('equal')
         self.ax.axis('off')
         plt.savefig(filename, dpi=300)
 
     def show(self):
         plt.show()
+        
+    def plot_great_circle(self, axis, radius, **kwargs):
+        circle = Circle((0, 0), radius, **kwargs)
+        self.ax.add_patch(circle)
+        pathpatch_2d_to_3d(circle, z=0, normal=axis)
+       
 
+
+from mpl_toolkits.mplot3d import art3d
+
+def rotation_matrix(v1,v2):
+    """
+    Calculates the rotation matrix that changes v1 into v2.
+    """
+    v1 = v1 / numpy.linalg.norm(v1)
+    v2 = v2 / numpy.linalg.norm(v2)
+
+    cos_angle=numpy.dot(v1,v2)
+    d=numpy.cross(v1,v2)
+    sin_angle=numpy.linalg.norm(d)
+
+    if sin_angle == 0:
+        M = numpy.identity(3) if cos_angle>0. else -numpy.identity(3)
+    else:
+        d/=sin_angle
+
+        eye = numpy.eye(3)
+        ddt = numpy.outer(d, d)
+        skew = numpy.array([[    0,  d[2], -d[1]],
+                            [-d[2],     0,  d[0]],
+                            [ d[1], -d[0],    0]], dtype=numpy.float64)
+
+        M = ddt + cos_angle * (eye - ddt) + sin_angle * skew
+
+    return M
+
+def pathpatch_2d_to_3d(pathpatch, z=0, normal = 'z'):
+    """
+    Transforms a 2D Patch to a 3D patch using the given normal vector.
+
+    The patch is projected into they XY plane, rotated about the origin
+    and finally translated by z.
+    """
+    if type(normal) is str: #Translate strings to normal vectors
+        index = "xyz".index(normal)
+        normal = numpy.roll((1,0,0), index)
+
+    path = pathpatch.get_path() #Get the path and the associated transform
+    trans = pathpatch.get_patch_transform()
+
+    path = trans.transform_path(path) #Apply the transform
+
+    pathpatch.__class__ = art3d.PathPatch3D #Change the class
+    pathpatch._code3d = path.codes #Copy the codes
+    pathpatch._facecolor3d = pathpatch.get_facecolor #Get the face color    
+
+    verts = path.vertices #Get the vertices in 2D
+
+    M = rotation_matrix(normal,(0, 0, 1)) #Get the rotation matrix
+
+    pathpatch._segment3d = numpy.array([numpy.dot(M, (x, y, 0)) + (0, 0, z) for x, y in verts])
+
+def pathpatch_translate(pathpatch, delta):
+    """
+    Translates the 3D pathpatch by the amount delta.
+    """
+    pathpatch._segment3d += delta
+    
 
 if __name__ == '__main__':
     import nvector
--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
+import os
 from setuptools import setup
 
+if os.environ.get("READTHEDOCS", False) == "True":
+    INSTALL_REQUIRES = []
+else:
+    INSTALL_REQUIRES = ['matplotlib']
+    
+
 setup(name='plots',
-      version='0.1',
-      description='Matplotlib abstraction to make plotting easier',
-      url='https://github.com/NiklasPhabian/plots',
-      author='Niklas Griessbaum',      
-      license='MIT', 
-      py_modules=['plots'],
-      zip_safe=False)
+    version='0.1',
+    description='Matplotlib abstraction to make plotting easier',
+    url='https://github.com/NiklasPhabian/plots',
+    author='Niklas Griessbaum',      
+    license='MIT', 
+    py_modules=['plots'],
+    zip_safe=False,
+    install_requires=INSTALL_REQUIRES
+)
